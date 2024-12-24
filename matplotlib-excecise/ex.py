2108f7dc64e6ac4ffdf810370ed3ee1dbaac65d0
michael <michaelchary081@gmail.com>
2024-12-24 17:56:07 +0330

    add-matplolib-pandas-py

---
+++ matplotlib-excecise/ex.py
@@ -0,0 +1,23 @@
+# import matplotlib.pyplot as plt
+
+# plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+# plt.xlabel('x-axis')
+# plt.ylabel('y-axis')
+# plt.title('Sample Plot')
+# plt.show()
+
+# import Tkinter
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+import matplotlib
+# matplotlib.use('TkAgg')
+
+df = pd.read_csv('data.csv')
+
+
+df.plot()
+
+
+plt.show()
--- pandas-excercise/ex.py
+++ pandas-excercise/ex.py
@@ -80,16 +80,331 @@
 # dataframe=pd.read_csv
 
 ##############################
+# import pandas as pd
+
+# data = {
+#     "name": ["michael", "pourya", "parham"],
+#     "family": ["Ch1", "Ch2", "Ch3"],
+#     "age": [22, 21, 21],
+# }
+
+# data_frame = pd.DataFrame(data)
+# print("########################")
+# print(data_frame)
+# print("########################")
+# print(data_frame.iloc[0,2])
+
+# import pandas as pd
+
+# print(pd.options.display.max_columns)
+# print(pd.options.display.max_rows)
+
+# pd.options.display.max_rows = 9999
+
+# data_frame = pd.read_csv('signals.csv')
+
+
+# print(data_frame)
+
+##################################################
+
+# import pandas as pd
+
+# data = {
+#     "Duration": {
+#         "0": 60,
+#         "1": 60,
+#         "2": 60,
+#         "3": 45,
+#         "4": 45,
+#         "5": 60
+#     },
+#     "Pulse": {
+#         "0": 110,
+#         "1": 117,
+#         "2": 103,
+#         "3": 109,
+#         "4": 117,
+#         "5": 102
+#     },
+#     "Maxpulse": {
+#         "0": 130,
+#         "1": 145,
+#         "2": 135,
+#         "3": 175,
+#         "4": 148,
+#         "5": 127
+#     },
+#     "Calories": {
+#         "0": 409,
+#         "1": 479,
+#         "2": 340,
+#         "3": 282,
+#         "4": 406,
+#         "5": 300
+#     }
+# }
+
+# data_frame = pd.DataFrame(data)
+# print(data_frame)
+
+###########################################
+
+# import pandas as pd
+
+# data_frame=pd.read_csv('signals.csv')
+
+# print(data_frame.head())
+
+# print(data_frame.tail())
+
+# import pandas as pd
+
+# data_frame=pd.read_csv('signals-test.csv')
+
+# data_frame.dropna(inplace=True)
+
+# print(data_frame)
+
+###################################
+
+# import pandas as pd
+
+# data = {
+#     'num1': [1, 2, 3, None],
+#     'num2': [4, 5, 6, None],
+#     'num3': [7, 8, 9, 10]
+# }
+
+# data_frame = pd.DataFrame(data)
+# print('before dropna!!')
+# print(data_frame)
+
+# change_date_frame = data_frame.dropna(inplace=True)
+# print('\ndrop-nan-data:')
+# print(change_date_frame)
+
+#######################################
+# import pandas as pd
+
+# data = {
+# 'num1': [1, 2, 3, None],
+# 'num2': [4, 5, 6, None],
+# 'num3': [7, 8, 9, 10]
+# }
+
+# data_frame = pd.DataFrame(data)
+# print('before dropna!!')
+# print(data_frame)
+# print("#################")
+# print(id(data_frame))
+# print("#################")
+
+
+# data_frame.dropna(inplace=True)
+
+
+# print('\ndrop-nan-data:')
+# print(data_frame)
+# print("#################")
+# print(id(data_frame))
+# print("#################")
+#######################################
+
+# import pandas as pd
+
+# data = {
+#     'num1': [1, 2, 3, 4],
+#     'num2': [5, 6, 7, 8],
+#     'num3': [9, 10, 11, 12],
+# }
+
+# data_frame = pd.DataFrame(data, index=[1, 2,3,4])
+
+# print(data_frame)
+
+#######################################
+
+# import pandas as pd
+
+# data = {
+#     'num1': [1, 2, 3, None],
+#     'num2': [4, 5, 6, None],
+#     'num3': [7, 8, 9, 10]
+# }
+
+# data_frame = pd.DataFrame(data)
+# data_frame.fillna(10, inplace=True)
+# print(data_frame)
+
+######################################
+
+# import pandas as pd
+
+# data_frame = pd.read_csv('data.csv')
+
+
+# mean = data_frame['Calories'].mean()
+
+# print('mean is:', mean)
+
+# data_frame.fillna(x, inplace=True)
+
+######################################
+
+# import pandas as pd
+
+# data_frame = pd.read_csv('data.csv')
+
+# meadian = data_frame['Calories'].median()
+
+# print("median is:", meadian)
+
+######################################
+
+# # # # # # import pandas as pd
+
+# # # # # # data_frame = pd.read_csv('order-date.csv')
+
+# # # # # # data_frame['Date'] = pd.to_datetime(data_frame['Date'],format='%Y/%m/%d',errors='coerce')
+
+# # # # # # print(data_frame.to_string())
+
+#####################################
+
+# import pandas as pd
+
+# df = pd.read_csv('data-2.csv')
+
+# df['Date'] = pd.to_datetime(df['Date'])
+
+# print(df.to_string())
+
+# import pandas as pd
+
+# data_frame=pd.read_csv("order-date.csv")
+
+# data_frame.dropna(subset=['Date'],inplace=True)
+
+# print(data_frame)
+
+####################################
+
+# import pandas as pd
+
+# dataframe = pd.read_csv('data-2.csv')
+
+# dataframe.loc[9, 'Duration'] = 45
+
+# # for index in dataframe.index:
+# #     if dataframe.loc(index, "Duration") > 120:
+# #         dataframe.loc(index, 'Duration') = 120
+
+# for x in dataframe.index:
+#   if dataframe.loc[x, "Duration"] > 120:
+#     dataframe.loc[x, "Duration"] = 120
+
+####################################
+
+# import pandas as pd
+
+# dataframe=pd.read_csv('duplicated.csv')
+
+# s=dataframe.duplicated()
+
+# print(s)
+
+####################################
+
+# import pandas as pd
+
+# data = {'A': [1, 2, 3, 4, 5, 6, 7, 8, 9.10, 11, 12, 13, 14]}
+
+# data_frame = pd.DataFrame(data)
+
+# print(data_frame.duplicated())
+# print("#####################################")
+
+# print(data_frame.to_string())
+# print('#####################################')
+
+# data_frame.drop_duplicates(inplace=True)
+
+# print('#####################################')
+# print(data_frame.to_string())
+# print('#####################################')
+
+# import pandas as pd
+
+# data_frame = pd.read_csv('data-3.csv')
+
+# print(data_frame.corr())
+
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+# data = {
+#     "age": [20, 25, 30, 35, 40, 45, 50],
+#     "weight": [90, 85, 80, 75, 70, 65, 60]
+# }
+
+# index = [1, 2, 3, 4, 5, 6, 7]
+
+# data_frame = pd.DataFrame(data, index=index)
+
+# data_frame.plot()
+
+# plt.show()
+
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+# data_frame=pd.read_csv('signals-test.csv')
+
+# data_frame.plot()
+
+# plt.show()
+
+##########################################
+
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+# data_frame=pd.read_csv('data.csv')
+
+# data_frame.plot()
+
+# plt.show()
+
+###########################################
+
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+# data_frame=pd.read_csv('data-for-plot.csv')
+
+# data_frame.plot()
+
+# plt.show()
+
+#####################################################
+
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+# data_frame = pd.read_csv('data-for-plot.csv')
+
+# data_frame.plot(kind='scatter',x = 'Duration', y = 'Calories')
+
+# plt.show()
+
+######################################################
+
 import pandas as pd
+import matplotlib.pyplot as plt
 
-data = {
-    "name": ["michael", "pourya", "parham"],
-    "family": ["Ch1", "Ch2", "Ch3"],
-    "age": [22, 21, 21],
-}
+data_frame = pd.read_csv('data-for-plot.csv')
 
-data_frame = pd.DataFrame(data)
-print("########################")
-print(data_frame)
-print("########################")
-print(data_frame.iloc[0,2])
+data_frame['Duration'].plot(kind='hist')
+
+plt.show()
