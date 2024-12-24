9103434586c74ad1c940b16f3b262963f05bea35
michael <michaelchary081@gmail.com>
2024-12-24 20:10:29 +0330

    add-numpy-packgae

---
+++ numpy-excecise/ex.py
@@ -0,0 +1,47 @@
+# import numpy
+
+# arr=numpy.array([1,2,3,4,5,6])
+
+# print(arr)
+
+#####################################
+
+# import numpy as np
+
+# arr=np.array([1,2,3,4,5,6])
+
+# print(arr)
+
+######################################
+
+# import numpy as np
+
+# print(np.__version__)
+
+################
+
+# import numpy as np
+
+# arr=np.array([1,2,3,4,5,6,7,8])
+
+# print(arr)
+
+# print(type(arr))
+
+###################
+
+# import numpy as np
+
+# arr=np.array(42)
+
+# print(arr)
+
+import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+print(arr[0][1])
+
+print(arr)
+
+####################
